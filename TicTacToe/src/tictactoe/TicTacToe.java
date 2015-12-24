d481f9c67dc3ce4bff8001376786e3843c20b461
Nexus <Nexus@Nexuss-MacBook-Pro-2.local>
2015-12-24 17:00:55 -0500

    First commit

---
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -0,0 +1,20 @@
+
+package tictactoe;
+
+import javax.swing.JFrame;
+
+public class TicTacToe {
+
+    
+    public static void main(String[] args) 
+    {
+        
+        JFrame ticTacToe = new TicTacToeFrame();
+        ticTacToe.setTitle("TicTacToe");
+        ticTacToe.setSize(600,600);
+        ticTacToe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ticTacToe.setLocationRelativeTo(null);
+        ticTacToe.setVisible(true);
+    }
+    
+}
